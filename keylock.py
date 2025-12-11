2fdedb54d32452b75396e34c06f868109decc9eb
lalacode <ilya-z1999@ya.ru>
2025-12-11 11:06:40 +0300

    Variable escape keys

--- keylock.py
+++ keylock.py
@@ -1,11 +1,22 @@
 from pynput import keyboard
 
+# keyboard.Key.ctrl_l
+# keyboard.KeyCode.from_char("q")
+escape_keys = {keyboard.Key.esc}
+pressed_keys = set()
+
 def on_press(key):
-    # Stop listener on pressing Esc key
-    if key == keyboard.Key.esc:
-        print("Escape character detected, exiting...")
+    pressed_keys.add(key)
+    print("You pressed:", key)
+    if all(k in pressed_keys for k in escape_keys):
         return False
 
-with keyboard.Listener(on_press=on_press, suppress=True) as listener:
-    print("Keyboard is locked, press escape character to remove the lock!")
+def on_release(key):
+    if key in pressed_keys:
+        pressed_keys.remove(key)
+
+with keyboard.Listener(on_press=on_press, on_release=on_release, suppress=True) as listener:
+    print("Keyboard is locked, press " + str(escape_keys) + " to remove the lock!")
     listener.join()  # Will block until listener is stopped by returning False
+    print("Escape character detected " + str(escape_keys) + ", exiting...")
+
