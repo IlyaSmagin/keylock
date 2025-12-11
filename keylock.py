141bed9a0e3acad1bef0dc03dd8dd97c79c0a682
lalacode <ilya-z1999@ya.ru>
2025-12-11 11:08:26 +0300

    Format pressed keys

--- keylock.py
+++ keylock.py
@@ -5,9 +5,19 @@ from pynput import keyboard
 escape_keys = {keyboard.Key.esc}
 pressed_keys = set()
 
+def format_keys(keys):
+    pressed_keys_names = []
+    for key in keys:
+        if hasattr(key, "char") and key.char:
+            pressed_keys_names.append(key.char)
+        else:
+            pressed_keys_names.append(key.name)
+    formatted_keys = " + ".join(pressed_keys_names)
+    return formatted_keys
+
 def on_press(key):
     pressed_keys.add(key)
-    print("You pressed:", key)
+    print("You pressed:", format_keys(pressed_keys))
     if all(k in pressed_keys for k in escape_keys):
         return False
 
@@ -16,7 +26,6 @@ def on_release(key):
         pressed_keys.remove(key)
 
 with keyboard.Listener(on_press=on_press, on_release=on_release, suppress=True) as listener:
-    print("Keyboard is locked, press " + str(escape_keys) + " to remove the lock!")
+    print("Keyboard is locked, press " + format_keys(escape_keys) + " to remove the lock!")
     listener.join()  # Will block until listener is stopped by returning False
-    print("Escape character detected " + str(escape_keys) + ", exiting...")
-
+    print("Escape character detected (" + format_keys(escape_keys) + "), exiting...")
