1cfdba7c64c9bb4d8cb223290194582060788d1b
lalacode <ilya-z1999@ya.ru>
2025-12-11 10:01:20 +0300

    Start/stop console indicators

--- keylock.py
+++ keylock.py
@@ -3,7 +3,9 @@ from pynput import keyboard
 def on_press(key):
     # Stop listener on pressing Esc key
     if key == keyboard.Key.esc:
+        print("Escape character detected, exiting...")
         return False
 
 with keyboard.Listener(on_press=on_press, suppress=True) as listener:
+    print("Keyboard is locked, press escape character to remove the lock!")
     listener.join()  # Will block until listener is stopped by returning False
