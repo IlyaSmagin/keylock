65d9a8f320e57554bacc67fcf9ad79907a94b17b
lalacode <ilya-z1999@ya.ru>
2025-12-14 17:55:38 +0300

    reused selector from helper functions in js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 async function start() {
   let res = await window.pywebview.api.start();
-  document.getElementById("status").innerText = "is " + res;
-  document.getElementById("currentlyPressed").innerText = "No keys are pressed";
+  update_listener_status(res);
+  update_pressed_keys("No keys are pressed");
 }
 async function stop() {
   let res = await window.pywebview.api.stop();
-  document.getElementById("status").innerText = "is " + res;
-  document.getElementById("currentlyPressed").innerText = "Not monitoring keys presses";
+  update_listener_status(res);
+  update_pressed_keys("Not monitoring key presses");
+}
+function update_listener_status(status_string) {
+  document.getElementById("status").innerText = "is " + status_string;
 }
 function update_pressed_keys(keys_string){
   document.getElementById("currentlyPressed").innerText = keys_string;
