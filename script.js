51ba8495c210e547cf3151f563b3701a57895bad
lalacode <ilya-z1999@ya.ru>
2025-12-18 13:56:12 +0300

    Key to kbd elements and some js  state and backslash escape refactor

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 async function start() {
   let res = await window.pywebview.api.start();
   update_listener_status(res);
-  update_pressed_keys("No keys are pressed");
+  set_pressed_keys_placeholder("No keys are pressed");
 }
 async function stop() {
   let res = await window.pywebview.api.stop();
   update_listener_status(res);
-  update_pressed_keys("Not monitoring key presses");
+  set_pressed_keys_placeholder("Not monitoring key presses");
 }
 function update_listener_status(status_string) {
   document.getElementById("status").innerText = "is " + status_string;
 }
-function update_pressed_keys(keys_string){
+function set_pressed_keys_placeholder(string) {
+  document.getElementById("lastPressed").innerText = string;
+}
+async function update_pressed_keys(keys_string){
+  // let debug = await window.pywebview.api.console(keys_string);
   document.getElementById("lastPressed").innerText = "Last pressed key is " + keys_string;
-  keys_string.split(" + ").map(key =>
-    document.querySelectorAll(".key-"+keys_string).forEach(triggeredClass =>
-      triggeredClass.classList.toggle("key-active"))
-  );
+
+  keys_string.split(" + ").map(key => {
+    key = CSS.escape(key);
+    document.querySelectorAll(`kbd[data-key='${key}'], kbd[data-alt='${key}']`).
+      forEach(triggeredClass =>
+        triggeredClass.classList.toggle("key-active"))
+  });
   }
 
 document.getElementById("startBtn").addEventListener("click", start);
