65d88b55182ed489daff1add800acefeeb48d052
Ilya Smagin <ilya-z1999@ya.ru>
2025-12-25 14:33:32 +0300

    Refactoring js to extract common logic

--- script.js
+++ script.js
@@ -3,46 +3,49 @@ async function start() {
   update_listener_status(res);
   set_pressed_keys_placeholder("No keys are pressed");
 }
+
 async function stop() {
   let res = await window.pywebview.api.stop();
   update_listener_status(res);
   set_pressed_keys_placeholder("Not monitoring key presses");
 }
+
 function update_listener_status(status_string) {
   document.getElementById("status").innerText = "is " + status_string;
 }
+
 function set_pressed_keys_placeholder(string) {
   document.getElementById("lastPressed").innerText = string;
 }
-async function update_pressed_keys(keys_string){
-  // let debug = await window.pywebview.api.console(keys_string);
-  document.getElementById("lastPressed").innerText = "Last pressed key is " + keys_string;
-  }
-async function update_keys_on_press(keys_string) {
-  update_pressed_keys(keys_string);
-  keys_string.split(" + ").map(key => {
-    key = CSS.escape(key);
-    document.querySelectorAll(`kbd[data-key='${key}'], kbd[data-alt='${key}']`).
-      forEach(triggeredClass =>
-        triggeredClass.classList.add("key-active"))
-  });
+
+function update_keys_on_press(keys_string) {
+  set_pressed_keys_placeholder("Last pressed key is " + keys_string);
+  update_key_classes("add", keys_string, "key-active");
 }
-async function update_keys_on_release(keys_string) {
-  update_pressed_keys(keys_string);
-  keys_string.split(" + ").map(key => {
-    key = CSS.escape(key);
-    document.querySelectorAll(`kbd[data-key='${key}'], kbd[data-alt='${key}']`).
-      forEach(triggeredClass =>
-        triggeredClass.classList.remove("key-active"))
-  });
+
+function update_keys_on_release(keys_string) {
+  set_pressed_keys_placeholder("Last pressed key is " + keys_string);
+  update_key_classes("remove", keys_string, "key-active");
 }
+
 function highlight_escape_keys(keys_string) {
-  keys_string.split(" + ").map(escape_key => {
-    document.querySelectorAll(`kbd[data-key='${escape_key}'], kbd[data-alt='${escape_key}']`).
-      forEach(triggeredClass =>
-        triggeredClass.classList.add("key-escape"))
+  update_key_classes("add", keys_string, "key-escape");
+}
+
+function update_key_classes(action, keys_string, class_name) {
+  keys_string.split(" + ").forEach(key => {
+    const key = CSS.escape(key);
+    document.querySelectorAll(`kbd[data-key='${key}'], kbd[data-alt='${key}']`)
+      .forEach(triggered_class => {
+        if (action === 'add') {
+          triggered_class.classList.add(class_name);
+        } else if (action === 'remove') {
+          triggered_class.classList.remove(class_name);
+        }
+      });
   });
 }
+
 
 document.getElementById("startBtn").addEventListener("click", start);
 document.getElementById("stopBtn").addEventListener("click", stop);
